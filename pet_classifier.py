3f88147a1717d28a84da684b382684f59eb9574c
Brian Hemen <brianhemen@gmail.com>
2020-06-21 15:52:50 -0700

    Changed train_generator batch size to 16 and added callbacks to end training early

--- pet_classifier.py
+++ pet_classifier.py
@@ -1,9 +1,16 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.optimizers import RMSprop
+import keras
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get('accuracy') > 0.99:
+            print('\n\nAchieved 99% accuracy. Stopping training.\n')
+            self.model.stop_training=True
+        
 
 
-#import keras
+
 imgDir = './pet-classifier-images/'
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -54,16 +61,20 @@ model.compile(
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
+
 train_datagen = ImageDataGenerator(rescale=1.0/255.0)
 train_generator = train_datagen.flow_from_directory(
     imgDir,
     target_size=(300,300),
-    batch_size=64,
+    batch_size=16,
     class_mode='binary'
 )
+
+callbacks = myCallback()
 history = model.fit(
     train_generator,
     steps_per_epoch=8,
     epochs=20,
     verbose=2,
+    callbacks=[callbacks]
 )
